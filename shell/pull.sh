044c713a5b2277ce29f3b15889b8ebff661934a6
hzl <huangzilong@sinozo.com>
2025-09-04 13:41:07 +0800

    update pull.sh

--- shell/pull.sh
+++ shell/pull.sh
@@ -1,120 +1,165 @@
 #!/usr/bin/env bash
 set -e
 
-script_dir="$(cd "$(dirname "$0")" && pwd)"
-echo "当前目录: $script_dir"
+urlencode() {
+  local string="${1}"
+  local length=${#string}
+  local encoded=""
+  local pos char
 
+  for (( pos = 0 ; pos < length ; pos++ )); do
+     char=${string:$pos:1}
+     case "$char" in
+        [a-zA-Z0-9.~_-]) encoded+="$char" ;;
+        *) encoded+=$(printf '%%%02X' "'$char") ;;
+     esac
+  done
+  echo "$encoded"
+}
 
+workFolder="/home/ubuntu/work/"
 
-echo "输入项目代号"
+read -p "请输入项目代号: " projectCode
+projectFolder="$workFolder$projectCode"
+if [ ! -e "$projectFolder" ] || [ ! -d "$projectFolder" ]; then
+    mkdir -p "$projectFolder"
+# else
+    # echo "$projectCode 项目已存在，不适用此脚本"
+    # exit 0
+fi
+echo "--------------"
+echo "项目相关的所有文件都存放在此文件夹：$projectFolder"
+echo "--------------"
+
+read -p "创建新的签名文件？yes/no:" generaSign
+if [ "$generaSign" = "yes" ]; then
+    while true; do
+        read -p "请输入alias:" alias
+        if [ -n "$alias" ]; then
+            break
+        fi
+    done
+    while true; do
+        read -p "请输入storePassword:" store_password
+        if [ -n "$store_password" ]; then
+            break
+        fi
+    done
+    while true; do
+        read -p "请输入KeyPassword，不输默认和Password相同:" key_password
+        if [ -n "$key_password" ]; then
+            break
+        else
+            key_password="$store_password"
+            break
+        fi
+    done
+    while true; do
+        read -p "请输入文件名，可以指定路径:" -e sign_path
+        if [ -e "$sign_path" ] && [ -f "$sign_path" ]; then
+            echo "请输入文件名，可以指定路径，文件已存在$sign_path"
+        elif [ -n "$sign_path" ]; then
+            break
+        fi
+    done
+    echo "keytool -genkeypair -v -alias $alias -keyalg RSA -keysize 2048 -validity 36500 -keypass $key_password -keystore $sign_path -storepass $store_password"
+    keytool -genkeypair -v -alias "$alias" -keyalg RSA -keysize 2048 -validity 36500 -keypass "$key_password" -keystore "$sign_path" -storepass "$store_password"
+    generaSignPath=$(realpath "$sign_path")
+    echo "新创建的签名文件路径:$generaSignPath"
+fi
+
 while true; do
-    read project_code
-    # echo "你输入的是$project_code"
-    if [ -n "$project_code" ]; then
-        break  # 退出循环，因为路径有效
-    else
-        echo "请输入项目代号！项目代号: $project_code"
-    fi
-done
-
-
-echo "输入打包脚本所在路径"
-while true; do
-    read -e sh_path
-    # echo "你输入的是$sh_path"
-    if [ -e "$sh_path" ] && [ -f "$sh_path" ]; then
-        break  # 退出循环，因为路径有效
-    else
-        echo "请输入正确的路径！没找到打包脚本,路径: $sh_path"
-    fi
-done
-
-echo "输入配置文件夹zip所在路径"
-while true; do
-    read -e zip_path
-    # echo "你输入的是$zip_path"
+    read -p "请输入配置文件夹压缩包路径:" -e zip_path
     if [ -e "$zip_path" ] && [ -f "$zip_path" ]; then
-        break  # 退出循环，因为路径有效
+        break
     else
-        echo "请输入正确的路径！没找到配置文件夹zip,路径: $zip_path"
+        echo "配置文件夹压缩包不存在"
     fi
 done
+configFolderName=$(zipinfo -1 "$zip_path" | head -n 1 | sed 's:/$::')
+unzip "$zip_path" -d "$projectFolder"
+if [ -n "$generaSignPath" ]; then
+    cp -rf "$generaSignPath" "$projectFolder/$configFolderName"
+fi
+flavorName=$(grep "flavor=" "$projectFolder/$configFolderName/$configFolderName.properties" | cut -d'=' -f2)
+if [ -z "$flavorName" ]; then
+    echo "$projectFolder/$configFolderName/$configFolderName.properties找不到flavor属性"
+    exit 0
+fi
 
-echo "输入项目源码存放路径"
 while true; do
-    read -e project_dir
-    # echo "你输入的是$project_dir"
-    if [ -n "$project_dir" ]; then
+    read -p "请输入git仓库地址:" git_url
+    if [ -n "$git_url" ]; then
         break  # 退出循环，因为路径有效
-    else
-        echo "请输入正确的路径！路径: $project_dir"
     fi
 done
-
-echo "输入编译成功的aab和混淆文件导出路径"
-while true; do
-    read -e bundle_dir
-    # echo "你输入的是$bundle_dir"
-    if [ -n "$bundle_dir" ]; then
-        break  # 退出循环，因为路径有效
-    else
-        echo "请输入正确的路径！路径: $bundle_dir"
-    fi
-done
-
-
-
-if [ ! -e "$project_code" ] || [ ! -d "$project_code" ]; then
-    mkdir -p "$project_code"
-fi
-if [ ! -e "$project_dir" ] || [ ! -d "$project_dir" ]; then
-    mkdir -p "$project_dir"
-fi
-if [ ! -e "$bundle_dir" ] || [ ! -d "$bundle_dir" ]; then
-    mkdir -p "$bundle_dir"
-fi
-
-unzip $zip_path -d $project_code
-cp -f $sh_path $project_code
-
-
-yes | sdkmanager --sdk_root=$ANDROID_HOME --licenses
-
-echo '输入git源地址'
-while true; do
-    read git_remote
-    # echo "你输入的是$git_remote"
-    if [ -n "$git_remote" ]; then
-        break  # 退出循环，因为路径有效
-    else
-        echo "请输入git源地址！ $git_remote"
-    fi
-done
-
-echo '输入分支名称，不输默认拉取主分支'
-read git_brunch
-#echo "你输入的是$git_brunch"
-
-echo '输入git token（可选）'
-read git_token
-#echo "你输入的是$git_token"
-
-if [ -n "$git_token" ]; then
-    git_full_remote=$(sed -E "s#(http[s]?://)#\1${git_token}@#" <<< "$git_remote")
-else
-    git_full_remote="$git_remote"
-fi
-echo "$git_full_remote"
-
-cd $project_dir
-
+read -p "输入分支名称，不输默认拉取主分支:" git_brunch
 if [ -z "$git_brunch" ]; then
-    git clone $git_full_remote
+    codeFolderName="$(basename -s .git $git_url)"
 else
-    git clone -b $git_brunch $git_full_remote
+    codeFolderName="$(basename -s .git $git_url)_$git_brunch"
 fi
 
+echo "设置git免登录操作"
+echo "0:不设置"
+echo "1:设置token免登录"
+echo "2:设置账号密码免登录"
+while true; do
+    read -p "请输入选项(0,1,2):" git_option
+    if [[ "$git_option" =~ ^[0-2]$ ]]; then
+        break
+    fi
+done
+if [ "$git_option" -eq 1 ]; then
+    while true; do
+        read -p "请输入git token:" git_token
+        if [ -n "$git_token" ]; then
+            break  # 退出循环，因为路径有效
+        fi
+    done
+    git_full_remote=$(sed -E "s#(http[s]?://)#\1${git_token}@#" <<< "$git_url")
+elif [ "$git_option" -eq 2 ]; then
+    while true; do
+        read -p "请输入git 用户名:" git_username
+        if [ -n "$git_username" ]; then
+            break  # 退出循环，因为路径有效
+        fi
+    done
+    while true; do
+        read -p "请输入git 密码:" git_pwd
+        if [ -n "$git_pwd" ]; then
+            break  # 退出循环，因为路径有效
+        fi
+    done
+    encoded_username=$(urlencode "$git_username")
+    encoded_pwd=$(urlencode "$git_pwd")
+    git_full_remote=$(sed -E "s#(http[s]?://)#\1${encoded_username}:${encoded_pwd}@#" <<< "$git_url")
+else
+    git_full_remote="$git_url"
+fi
+if [ -z "$git_brunch" ]; then
+    echo "git clone $git_full_remote $projectFolder/$codeFolderName"
+    git clone $git_full_remote "$projectFolder/$codeFolderName"
+else
+    echo "git clone -b $git_brunch $git_full_remote $projectFolder/$codeFolderName"
+    git clone -b $git_brunch $git_full_remote "$projectFolder/$codeFolderName"
+fi
 
-echo "运行 'sh $project_code/$(basename "$sh_path")' 开始打包吧"
+while true; do
+    read -p "请输入打包文件aab的压缩密码:" aabZipPwd
+    if [ -n "$aabZipPwd" ]; then
+        break  # 退出循环，因为路径有效
+    fi
+done
+encodedProjectCode=$(urlencode "$projectCode")
+encodedProjectPath=$(urlencode "$projectFolder/$codeFolderName")
+encodedConfigPath=$(urlencode "$projectFolder/$configFolderName")
+encodedFlavorName=$(urlencode "$flavorName")
+encodedAabZipPwd=$(urlencode "$aabZipPwd")
+encodedSavePath=$(urlencode "$projectFolder/bundleZip")
+bundleUrl="https://bundle-worker.a807217059.workers.dev/?projectCode=$encodedProjectCode&projectPath=$encodedProjectPath&configPath=$encodedConfigPath&flavorName=$encodedFlavorName&zipPwd=$encodedAabZipPwd&savePath=$encodedSavePath"
+# echo "$bundleUrl"
+wget -P "$projectFolder" --content-disposition "$bundleUrl"
+downloadPath=$(ls -t "$projectFolder" | head -n1)
 
-
+echo "运行打包脚本，开始打包吧！sh $projectFolder/$downloadPath"
